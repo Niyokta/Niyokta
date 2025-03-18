fcc18a5142eecae0c611631380da3bcc286334e9
SahayakSharma <sahayaksharma6@gmail.com>
2025-03-18 11:44:19 +0530

    responsive : fixtures

--- src/components/Dashboard/RecentProjects.tsx
+++ src/components/Dashboard/RecentProjects.tsx
@@ -2,6 +2,7 @@
 import React from "react";
 import { useAppSelector } from "@/lib/reduxHooks";
 import ProjectsMap,{NoProjects} from "./ProjectsMap";
+import { primary_accent_text } from "@/resource/theme";
 export default function RecentProjects(){
     const projects=useAppSelector(state=>state.user.projects)
     const username=useAppSelector(state=>state.user.userName)
@@ -9,7 +10,7 @@ export default function RecentProjects(){
     const topfive=sortedProjects.slice(0,5);
     return(
         <div className="md:mt-[50px]">
-            <p className="text-[15px] md:text-[30px] pt-[30px] font-bold md:text-center">Recent Projects</p>
+            <p className="text-[15px] md:text-[30px] pt-[30px] font-bold md:text-center" style={{color:primary_accent_text}}>Recent Projects</p>
             {
                 username=="" ? <NoProjects/> : <ProjectsMap projects={topfive}/>
             }
