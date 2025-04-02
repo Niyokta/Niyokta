1a5b90c088b7bd45d35cfc6c5b058ae7200f036b
SahayakSharma <sahayaksharma6@gmail.com>
2025-04-02 15:33:12 +0530

    refactor : projects

---
+++ src/lib/types/FilterTypes.ts
@@ -0,0 +1,15 @@
+export type skillFilterType={
+    filterTitle:String,
+    filterKey:String,
+    category:String
+}
+
+export type categoryFilterType={
+    filterTitle:String,
+    filterKey:String,
+}
+
+export type filterType={
+    skillFilter:String[],
+    categoryFilter:String[]
+  }
